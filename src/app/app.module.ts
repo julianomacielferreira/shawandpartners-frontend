2aa1e09a9494aedffa6f34e79a8d07cb4053d786
Juliano Maciel Ferreira <ju.maciel.ferreira@gmail.com>
2019-11-05 08:12:30 -0300

    Add components and import modules in AppModule.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,17 +22,21 @@
  * THE SOFTWARE.
  */
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListUsersComponent } from './list-users/list-users.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListUsersComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
