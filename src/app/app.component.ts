7d66cd05331c12033b26520379b86368a28ac9b9
Juliano Maciel Ferreira <ju.maciel.ferreira@gmail.com>
2019-11-05 08:12:57 -0300

    Remove unused code.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,10 +24,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'mlocks-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'shawandpartners-frontend';
+
+  constructor() { }
 }
